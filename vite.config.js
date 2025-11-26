56a5cdcf4fb900ff3ec18f2c5e9adfdbeecd647b
Fpierr <fanuelp9@gmail.com>
2025-11-26 20:40:37 +0100

    fix err pg page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Wilbeauty_v2/',
   plugins: [
     react(),
     tailwindcss(),
