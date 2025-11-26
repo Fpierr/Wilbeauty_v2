e86b27063d21a88b3df5844649d4aade1a2167ac
Fpierr <fanuelp9@gmail.com>
2025-11-26 20:56:02 +0100

    responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <img src={logo} alt="Logo" className="h-10" />
           </a>
         </div>
-        <div className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0 md:ml-auto">
+        <div className="absolute left-1/2 md-translate-x-1/2 md:static md:translate-x-0 md:ml-auto">
           <a 
             href="mailto:wilbeauty7@gmail.com"
             className="text-[var(--color-accent)] no-underline text-base md:text-xl font-bold hover:underline"
