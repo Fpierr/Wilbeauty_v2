3a7d786f6be2fc22b510b45c9b5f8912736bba68
Fpierr <fanuelp9@gmail.com>
2026-02-26 18:27:37 +0100

    edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 // import accueilImage from '../assets/accueil.png';
-import iconWhatsapp from '../assets/icon_whatsp.png';
-import iconTel from '../assets/icon_tel.png';
+// import iconWhatsapp from '../assets/icon_whatsp.png';
+// import iconTel from '../assets/icon_tel.png';
 
 const Hero = () => {
   const [modalData, setModalData] = useState({ open: false, type: '' });
@@ -15,11 +15,11 @@ const Hero = () => {
   const confirmAction = () => {
     if (modalData.type === 'whatsapp') {
       window.open(
-        "https://wa.me/18099929258?text=Bonjour,%0AVia%20le%20site%20WilBeauty.com,%20je%20souhaite%20passer%20une%20commande.",
+        "https://wa.me/numberphone?text=Bonjour,%0AVia%20le%20site%20WilBeauty.com,%20je%20souhaite%20passer%20une%20commande.",
         "_blank"
       );
     } else if (modalData.type === 'call') {
-      window.open("tel:+18099929258", "_self");
+      window.open("tel:+numberphone", "_self");
     }
     closeModal();
   };
@@ -36,7 +36,7 @@ const Hero = () => {
             Offrez à votre cuir chevelu une nutrition profonde et durable avec un soin 100 % naturel. 
             Disponible en huile et en pomade.
           </p>
-          <p className="text-lg mb-7">
+          {/* <p className="text-lg mb-7">
             Cliquez ci-dessous pour commender par Téléphone :
           </p>
           
@@ -62,7 +62,7 @@ const Hero = () => {
               />
             </button>
           </div>
-          
+           */}
           <a 
             href="#produit" 
             className="inline-block py-2.5 px-5 bg-[var(--color-button)] text-white no-underline font-bold rounded-[5px] mb-7 transition-colors duration-300 hover:bg-[var(--color-button-hover)]"
